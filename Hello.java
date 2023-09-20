f5cb4713cf0e17e0e81411e58c10bf8fb48b6454
saryasajeev <145546323+saryasajeev@users.noreply.github.com>
2023-09-20 14:27:23 +0530

    My First Java Project

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**************************************************************************************************************
+ * File        : Hello.java
+ * Description : My First Java Program
+ * Author      : Sarya Sajeev
+ * Date        : 20/09/2023
+ ****************************************************************************************************************/
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Hello Java");
+	}
+
+}
